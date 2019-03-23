ae8baf475eeb80bc32c621de6808c075972049cf
Me <me@example.com>
2019-03-23 13:04:42 +0100

    init

---
+++ BaseDemo.js
@@ -0,0 +1,25 @@
+
+class BaseDemo extends Phaser.Scene {
+
+    constructor() {
+        super();
+        this.BACKGROUND_URL = "resource/background.png";
+    }
+    
+    preload() {
+        this.plugins.installScenePlugin('dragonBones', dragonBones.phaser.plugin.DragonBonesScenePlugin, 'dragonbone', this);
+        this.load.image(this.BACKGROUND_URL, this.BACKGROUND_URL);
+    }
+
+    create() {
+        this.add.image(0, 0, this.BACKGROUND_URL);
+    }
+
+    createText(str) {
+        const style = { fontSize: 18, color: "#FFFFFF", align: "center" };
+        const text = this.add.text(this.cameras.main.centerX, this.cameras.main.height - 100, str, style);
+        text.setOrigin(.5, .5);
+        return text;
+    }
+
+}
